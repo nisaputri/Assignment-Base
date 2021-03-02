4c94e1db7fdf0004c3357d96c307dbaaa3ba0aaa
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-02 12:42:04 -0500

    Connected to data and filtered
    followed class example to get working connection

--- public/script.js
+++ public/script.js
@@ -1,10 +1,29 @@
-async function onLoadPage() {
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    const json = await data.json();
-    const user_input = document.querySelector('input[type = 'text']');
-    user_input.addEventListener('input', (e) => {
-        
+async function windowActions() {
+    console.log("Window Loaded");
+
+    const search = document.querySelector('#search');
+    const form = document.querySelector('.searchForm');
+
+    const request = await fetch('/api');
+    const data = await request.json();
+
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        console.log('submitted query is', search.value);
+        console.log('data quantity (should be 1000)', data.length)
+        const display = data.filter((record) => {
+            const val = search.value;
+            const zip = record.zip;
+            return zip.includes(val)
+        });
+        console.log('filtered data,', display)
     })
+
+
+
+
+
 }
 
-window.onload = onLoadPage;
+window.onload = windowActions;
--- server.js
+++ server.js
@@ -22,9 +22,11 @@ app.use((req, res, next) => {
 
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
   })
   .post(async (req, res) => {
     console.log('POST request detected');
