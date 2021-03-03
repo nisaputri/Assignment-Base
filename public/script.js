4ae944b291c43aae32aae08510da272424027dea
nisaputri <pnisa1911@gmail.com>
2021-03-02 23:41:39 -0500

    UPDATE - The JS file for searching names, CSS file for coloring gradients, and HTML file for div

--- public/script.js
+++ public/script.js
@@ -1,42 +1,3 @@
-<<<<<<< Updated upstream
-const data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const user_input = document.querySelector('SearchBar');
-const restaurant_list = document.querySelector('.ListOfRestaurants');
-const restaurant_type = [];
-    
-fetch(endpoint) 
-  .then(res => res.json()) 
-  .then(data => restaurant_type.push(...data));
-
-function findMatches(wordToMatch, restaurant_type) {
-    return restaurant_type.filter(place => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.restaurant_type.match(regex) || place.zipcode.match(regex);
-    });
-  }
-
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
-
-  function displayMatches() {
-    const matchArray = findMatches(this.value, restaurant_type);
-    const html = matchArray.map(place => {
-      const regex = new RegExp(this.value, 'gi');
-      const restaurantName = place.restaurant.replace(regex, `<span class="hl">${this.value}</span>`);
-      const Zipcode = place.zipcode.replace(regex, `<span class="hl">${this.value}</span>`);
-      return `
-        <li>
-          <span class="name">${restaurantName}, ${Zipcode}</span>
-          
-        </li>`
-    }).join('');
-    suggestions.innerHTML = html;
-  }
-
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
-=======
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const restaurants = [];
 
@@ -70,4 +31,3 @@ const searchInput = document.querySelector('.SearchBar')
 const suggestions = document.querySelector('.ListOfRestaurants')
 
 searchInput.addEventListener('change', displayMatches);
->>>>>>> Stashed changes
