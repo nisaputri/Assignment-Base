b54028d1b123871572101de0918f90aab8e3a92c
khanalaakriti <66918681+khanalaakriti@users.noreply.github.com>
2021-03-05 14:45:37 -0500

    Final Updates

--- public/script.js
+++ public/script.js
@@ -1,37 +1,51 @@
-const data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const user_input = document.querySelector('.SearchBar');
-const restaurant_list = document.querySelector('.ListOfRestaurants');
-const restaurant_type = [];
-    
-fetch(data) 
-  .then(res => res.json()) 
-  .then(data => restaurant_type.push(...data));
-
-function findMatches(wordToMatch, restaurant_type) {
-    return restaurant_type.filter(place => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.restaurant_type.match(regex) || place.zipcode.match(regex);
+async function windowAction() {
+ 
+    const request = await fetch('/api')
+    const restaurants = await request.json()
+  
+    function findMatches(wordToMatch, restaurants) {
+      return restaurants.filter((place) => {
+        const regex = new RegExp(wordToMatch, "gi");
+        return (
+          place.name.match(regex) ||
+          place.zip.match(regex) ||
+          place.category.match(regex)
+        );
+      });
+    }
+  
+    //this code shows the display that matches the user's input
+    function displayMatches(event) {
+      const matchArray = findMatches(event.target.value, restaurants);
+      const html = matchArray
+        .map((place) => {
+          return `<li>
+          <span class="name"><b>${place.name}</b></span><br>
+          <span class="category"><b>${place.category}</b></span>
+          <address><b>${place.address_line_1}</b><br>
+          <b>${place.city}</b><br>
+          <b>${place.state}</b><br>
+          <b>${place.zip}</b><address>
+          <b>${place.type}</b><address>
+          <b>${place.owner}</b><address>
+        </li>`;
+        })
+        .join("");
+      suggestions.innerHTML = html;
+    }
+  
+    const searchInput = document.querySelector(".SearchBar");
+    const suggestions = document.querySelector(".ListOfRestaurants");
+  
+    searchInput.addEventListener("keyup", (e) => {
+        console.log('LOADING...');
+      displayMatches(e);
     });
   }
+  
+  window.onload = windowAction;
 
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
+const searchInput = document.querySelector('.SearchBar')
+const suggestions = document.querySelector('.ListOfRestaurants')
 
-  function displayMatches() {
-    const matchArray = findMatches(this.value, restaurant_type);
-    const html = matchArray.map(place => {
-      const regex = new RegExp(this.value, 'gi');
-      const restaurantName = place.restaurant.replace(regex, `<span class="hl">${this.value}</span>`);
-      const Zipcode = place.zipcode.replace(regex, `<span class="hl">${this.value}</span>`);
-      return `
-        <li>
-          <span class="name">${restaurantName}, ${Zipcode}</span>
-          
-        </li>`
-    }).join('');
-    suggestions.innerHTML = html;
-  }
-
-  user_input.addEventListener('change', displayMatches);
-  user_input.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('change', displayMatches);
